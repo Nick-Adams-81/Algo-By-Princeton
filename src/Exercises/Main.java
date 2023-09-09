a370aaf1682d4e55b3769ee714a152750fee4acd
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-09 12:03:10 -0700

    quick find, connected, and union

---
+++ src/Exercises/Main.java
@@ -0,0 +1,17 @@
+package Exercises;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        UnionFind unionFind = new UnionFind(1);
+
+
+
+    }
+
+}
---
+++ src/Exercises/UnionFind.java
@@ -0,0 +1,15 @@
+package Exercises;
+
+public class UnionFind {
+
+    // "Connected" definition
+    // Reflective: p is connected to p.
+    // Symetric: if p is connected to q, then q is connected to p.
+    // Transitive: if p is connected to q and q is connected to r, then p is connected to r.
+
+    UnionFind(int N) {
+
+    }
+
+
+}
